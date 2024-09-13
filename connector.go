ccf70ef9901084de487a22346af301ba8194c15f
Parikshit Gothwal <parikshit.g@gopherslab.com>
2024-09-13 11:34:12 +0530

    feat: root connector

--- connector.go
+++ connector.go
@@ -1,4 +1,4 @@
-// Copyright © 2022 Meroxa, Inc. and Miquido
+// Copyright © 2022 Meroxa, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,13 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package elasticsearch
 
 import (
-	elasticsearch "github.com/conduitio-labs/conduit-connector-elasticsearch"
+	"github.com/conduitio-labs/conduit-connector-elasticsearch/destination"
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
-func main() {
-	sdk.Serve(elasticsearch.Connector)
+var Connector = sdk.Connector{
+	NewSpecification: Specification,
+	NewSource:        nil,
+	NewDestination:   destination.NewDestination,
 }
