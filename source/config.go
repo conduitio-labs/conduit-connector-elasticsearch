5b887786c06835167d374510c4fb0fef28edf1cd
Parikshit Gothwal <parikshit.g@gopherslab.com>
2024-10-02 15:37:08 +0530

    fix: config

--- source/config.go
+++ source/config.go
@@ -37,14 +37,12 @@ type Config struct {
 	ServiceToken string `json:"serviceToken"`
 	// SHA256 hex fingerprint given by Elasticsearch on first launch.
 	CertificateFingerprint string `json:"certificateFingerprint"`
-	// The name of the index to write the data to.
-	Index string `json:"index"`
-	// The name of the index's type to write the data to.
-	Type string `json:"type"`
+	// The name of the indexes to read data from.
+	Index []string `json:"index"`
+	// // The name of the index's type to write the data to.
+	// Type string `json:"type"`
 	// The number of items stored in bulk in the index. The minimum value is `1`, maximum value is `10 000`.
-	BulkSize uint64 `json:"bulkSize" default:"1000"`
-	// The maximum number of retries of failed operations. The minimum value is `0` which disabled retry logic. The maximum value is `255.
-	Retries uint8 `json:"retries" default:"0"`
+	BatchSize uint64 `json:"batchSize" default:"1"`
 }
 
 func (c Config) GetHost() string {
@@ -75,9 +73,9 @@ func (c Config) GetCertificateFingerprint() string {
 	return c.CertificateFingerprint
 }
 
-func (c Config) GetIndex() string {
-	return c.Index
-}
+// func (c Config) GetIndex() string {
+// 	return c.Index
+// }
 
 func (c Config) GetType() string {
 	return c.Type
